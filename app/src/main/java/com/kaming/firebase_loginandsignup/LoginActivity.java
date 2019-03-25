5f7083458c25e7838fb152e828a1cd7384295bc8
kaming54 <km20170101@gmail.com>
2019-03-25 16:36:15 +0800

    emptyValueHaveBug_OtherIsOk

--- app/src/main/java/com/kaming/firebase_loginandsignup/LoginActivity.java
+++ app/src/main/java/com/kaming/firebase_loginandsignup/LoginActivity.java
@@ -1,13 +1,59 @@
 package com.kaming.firebase_loginandsignup;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText id, pass;
+    private Button loginBtn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initView();
+    }
+
+    private void initView() {
+        id = (EditText) findViewById(R.id.id);
+        pass = (EditText) findViewById(R.id.password);
+        loginBtn = (Button) findViewById(R.id.login);
+        mAuth = FirebaseAuth.getInstance();
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = id.getText().toString();
+                String password = pass.getText().toString();
+                mAuth.signInWithEmailAndPassword(account, password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(LoginActivity.this, "登入成功", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent();
+                                intent.setClass(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(LoginActivity.this, "登入錯誤", Toast.LENGTH_LONG).show();
+                            }
+                        });
+            }
+        });
     }
 }
